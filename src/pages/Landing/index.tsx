4ba3be0f459ece54e6ff17b3f853b7670aacd14d
Gustavo Mori <gustavo.mori.peixoto@usp.br>
2020-08-06 19:39:46 -0300

    add tabs navigation

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -15,6 +15,10 @@ function Landing(){
     navigate('GiveClasses');
   }
 
+  function handleNavigateToStudyPages(){
+    navigate('Study')
+  }
+
   return (
       <View style={styles.container}>
         <Image source={landingImg} style={styles.banner}/>
@@ -26,7 +30,10 @@ function Landing(){
 
 
         <View style={styles.buttonsContainer}>
-          <RectButton style={[styles.button, styles.buttonPrimary]}>
+          <RectButton 
+            onPress={handleNavigateToStudyPages}
+            style={[styles.button, styles.buttonPrimary]}
+          >
             <Image source={studyIcon}/>
             <Text style={styles.buttonText}>Estudar</Text>
           </RectButton>
